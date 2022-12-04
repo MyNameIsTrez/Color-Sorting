be99b95cfd1b923926394300fd353555cb50de90
MyNameIsTrez <welfje@gmail.com>
2022-12-04 03:51:19 +0100

    Create LabInfo class and refactor everything

--- 2d/gpu-swap/Color-Sorting/Color-Sorting/Program.cs
+++ 2d/gpu-swap/Color-Sorting/Color-Sorting/Program.cs
@@ -1,137 +1,57 @@
-﻿using System;
-//using System.Globalization;
-//using System.Linq;
-//using CommunityToolkit.Diagnostics;
+﻿using Colourful;
 using ComputeSharp;
 using System.Drawing;
-//using System.Drawing.Common;
-//using System.Drawing.Imaging;
-using Colourful;
-
-//using OpenTK.Windowing.Common;
-//using ComputeSharp.Sample;
-/*
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.Diagnostics;
-using System.IO;
-*/
 
 internal class Program
 {
-    private static byte get_normalized_lab(double x, double min, double range)
-    {
-        return Convert.ToByte(((x - min) / range) * 255);
-    }
-    private static double get_denormalized_lab(double x, double min, double range)
-    {
-        return ((x / 255) * range) + min;
-    }
+    const string INPUT_IMAGE_PATH = "I:/Programming/Color-Sorting/2d/gpu-swap/Color-Sorting/Color-Sorting/palette.bmp";
+    const string OUTPUT_IMAGES_DIRECTORY_PATH = "I:/Programming/Color-Sorting/2d/gpu-swap/Color-Sorting/Color-Sorting";
 
     private static void Main(string[] args)
     {
-        var rgbWorkingSpace = RGBWorkingSpaces.sRGB;
-        var rgbToLab = new ConverterBuilder().FromRGB(rgbWorkingSpace).ToLab(Illuminants.D50).Build();
-        var labToRgb = new ConverterBuilder().FromLab(Illuminants.D50).ToRGB(rgbWorkingSpace).Build();
+        var LabInfo = new LabInfo();
 
-        
-        // Source: https://stackoverflow.com/a/19099064/13279557
-        double maxL = double.MinValue;
-        double maxA = double.MinValue;
-        double maxB = double.MinValue;
-        double minL = double.MaxValue;
-        double minA = double.MaxValue;
-        double minB = double.MaxValue;
-
-        for (double r = 0; r < 256; ++r)
-        {
-            Console.WriteLine(String.Format("{0}/255", r));
-            for (double g = 0; g < 256; ++g)
-                for (double b = 0; b < 256; ++b)
-                {
-                    var rgb = new RGBColor(r / 255, g / 255, b / 255);
-                    var lab = rgbToLab.Convert(rgb);
-
-                    maxL = Math.Max(maxL, lab.L);
-                    maxA = Math.Max(maxA, lab.a);
-                    maxB = Math.Max(maxB, lab.b);
-                    minL = Math.Min(minL, lab.L);
-                    minA = Math.Min(minA, lab.a);
-                    minB = Math.Min(minB, lab.b);
-                }
-        }
-
-        var rangeL = maxL - minL;
-        var rangeA = maxA - minA;
-        var rangeB = maxB - minB;
-
-        Console.WriteLine(String.Format("L in [{0}, {1}], range {2}", minL, maxL, rangeL));
-        Console.WriteLine(String.Format("a in [{0}, {1}], range {2}", minA, maxA, rangeA));
-        Console.WriteLine(String.Format("b in [{0}, {1}], range {2}", minB, maxB, rangeB));
-        
+        LabInfo.Print();
 
         Console.WriteLine("Turning input image into CIELab pixels...");
-        var palette = new Bitmap("I:/Programming/Color-Sorting/2d/gpu-swap/Color-Sorting/Color-Sorting/palette.bmp");
-        
-        var pixels = new Rgba32[palette.Height, palette.Width];
+        var palette = new Bitmap(INPUT_IMAGE_PATH);
 
-        /*var k = new RGBColor(1, 0, 1);
-        var l = rgbToLab.Convert(k);
-        var m = labToRgb.Convert(l);*/
+        var pixels = new Rgba32[palette.Height, palette.Width];
 
         for (int y = 0; y < palette.Height; y++)
         {
             for (int x = 0; x < palette.Width; x++)
             {
                 var pixel = palette.GetPixel(x, y);
-                //pixels[y, x] = new Rgba32(pixel.R, pixel.G, pixel.B);
-                //pixels[y, x] = new Rgba32(255, 127, 10);
 
                 var rgb = new RGBColor(Convert.ToDouble(pixel.R) / 255, Convert.ToDouble(pixel.G) / 255, Convert.ToDouble(pixel.B) / 255);
-                var lab = rgbToLab.Convert(rgb);
+                var lab = LabInfo.RGBToLab.Convert(rgb);
 
-                var normalized_L = get_normalized_lab(lab.L, minL, rangeL);
-                var normalized_A = get_normalized_lab(lab.a, minA, rangeA);
-                var normalized_B = get_normalized_lab(lab.b, minB, rangeB);
+                var normalized_L = GetNormalizedLab(lab.L, LabInfo.MinL, LabInfo.RangeL);
+                var normalized_A = GetNormalizedLab(lab.a, LabInfo.MinA, LabInfo.RangeA);
+                var normalized_B = GetNormalizedLab(lab.b, LabInfo.MinB, LabInfo.RangeB);
                 pixels[y, x] = new Rgba32(normalized_L, normalized_A, normalized_B);
-
-                /*var l = Convert.ToByte(lab.L + 128);
-                var rgba = new Rgba32(l, Convert.ToByte(lab.a + 128), Convert.ToByte(lab.b + 128));
-                pixels[y, x] = rgba;*/
             }
         }
 
-        
         for (int y = 0; y < palette.Height; y++)
         {
             for (int x = 0; x < palette.Width; x++)
             {
                 var pixel = pixels[y, x];
 
-                var L = get_denormalized_lab(pixel.R, minL, rangeL);
-                var A = get_denormalized_lab(pixel.G, minA, rangeA);
-                var B = get_denormalized_lab(pixel.B, minB, rangeB);
+                var L = GetDenormalizedLab(pixel.R, LabInfo.MinL, LabInfo.RangeL);
+                var A = GetDenormalizedLab(pixel.G, LabInfo.MinA, LabInfo.RangeA);
+                var B = GetDenormalizedLab(pixel.B, LabInfo.MinB, LabInfo.RangeB);
                 var lab = new LabColor(L, A, B);
-                var rgb = labToRgb.Convert(lab);
+                var rgb = LabInfo.LabToRGB.Convert(lab);
 
                 var denormalized_r = Convert.ToByte(Math.Clamp(rgb.R * 255, 0, 255));
                 var denormalized_g = Convert.ToByte(Math.Clamp(rgb.G * 255, 0, 255));
                 var denormalized_b = Convert.ToByte(Math.Clamp(rgb.B * 255, 0, 255));
                 pixels[y, x] = new Rgba32(denormalized_r, denormalized_g, denormalized_b);
-
-                /*var l = pixel.R - 128;
-                //var lab = new LabColor(Convert.ToDouble(l), Convert.ToDouble(pixel.G - 128), Convert.ToDouble(pixel.B - 128));
-                var lab = new LabColor(60, 94, -60);
-                var rgb = labToRgb.Convert(lab);
-                pixels[y, x] = new Rgba32(Convert.ToByte(rgb.R), Convert.ToByte(rgb.G), Convert.ToByte(rgb.B));*/
             }
         }
-        
 
         Console.WriteLine("Allocating pixels GPU buffer...");
         using var texture = GraphicsDevice.GetDefault().AllocateReadWriteTexture2D<Rgba32, float4>(pixels);
@@ -142,7 +62,81 @@ internal class Program
 
         //GraphicsDevice.GetDefault().For(texture.Width, texture.Height, new GrayscaleEffect(texture));
 
-        texture.Save("I:/Programming/Color-Sorting/2d/gpu-swap/Color-Sorting/Color-Sorting/1.png");
+        texture.Save(Path.Combine(OUTPUT_IMAGES_DIRECTORY_PATH, "1.png"));
+    }
+
+    private static byte GetNormalizedLab(double x, double min, double range)
+    {
+        return Convert.ToByte(((x - min) / range) * 255);
+    }
+    private static double GetDenormalizedLab(double x, double min, double range)
+    {
+        return ((x / 255) * range) + min;
+    }
+
+    class LabInfo
+    {
+        public IColorConverter<RGBColor, LabColor> RGBToLab;
+        public IColorConverter<LabColor, RGBColor> LabToRGB;
+
+        public double MinL;
+        public double MinA;
+        public double MinB;
+
+        private double MaxL = double.MinValue;
+        private double MaxA = double.MinValue;
+        private double MaxB = double.MinValue;
+
+        public double RangeL;
+        public double RangeA;
+        public double RangeB;
+
+        public LabInfo()
+        {
+            var rgbWorkingSpace = RGBWorkingSpaces.sRGB;
+            RGBToLab = new ConverterBuilder().FromRGB(rgbWorkingSpace).ToLab(Illuminants.D50).Build();
+            LabToRGB = new ConverterBuilder().FromLab(Illuminants.D50).ToRGB(rgbWorkingSpace).Build();
+
+            Calculate();
+        }
+
+        // Source: https://stackoverflow.com/a/19099064/13279557
+        private void Calculate()
+        {
+            MinL = double.MaxValue;
+            MinA = double.MaxValue;
+            MinB = double.MaxValue;
+
+            for (double r = 0; r < 256; ++r)
+            {
+                Console.WriteLine(String.Format("{0}/255", r));
+                for (double g = 0; g < 256; ++g)
+                    for (double b = 0; b < 256; ++b)
+                    {
+                        var rgb = new RGBColor(r / 255, g / 255, b / 255);
+                        var lab = RGBToLab.Convert(rgb);
+
+                        MinL = Math.Min(MinL, lab.L);
+                        MinA = Math.Min(MinA, lab.a);
+                        MinB = Math.Min(MinB, lab.b);
+
+                        MaxL = Math.Max(MaxL, lab.L);
+                        MaxA = Math.Max(MaxA, lab.a);
+                        MaxB = Math.Max(MaxB, lab.b);
+                    }
+            }
+
+            RangeL = MaxL - MinL;
+            RangeA = MaxA - MinA;
+            RangeB = MaxB - MinB;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(String.Format("L is always between [{0}, {1}], range {2}", MinL, MaxL, RangeL));
+            Console.WriteLine(String.Format("a is always between [{0}, {1}], range {2}", MinA, MaxA, RangeA));
+            Console.WriteLine(String.Format("b is always between [{0}, {1}], range {2}", MinB, MaxB, RangeB));
+        }
     }
 }
 
