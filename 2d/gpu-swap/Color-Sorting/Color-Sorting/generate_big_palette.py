d0764fcf16f555d39307d2d6849c158ca269b196
MyNameIsTrez <welfje@gmail.com>
2022-12-17 23:08:04 +0100

    Shuffle output pixels of generate_big_palette.py

--- 2d/gpu-swap/Color-Sorting/Color-Sorting/generate_big_palette.py
+++ 2d/gpu-swap/Color-Sorting/Color-Sorting/generate_big_palette.py
@@ -1,15 +1,27 @@
 ﻿from PIL import Image
+import numpy as np
 
 
 def main(duplicates_width, duplicates_height):
 	img = Image.open('palette.bmp')
 	img_w, img_h = img.size
-	background = Image.new('RGBA', (img_w * duplicates_width, img_h * duplicates_height))
+
+	background = Image.new('RGB', (img_w * duplicates_width, img_h * duplicates_height))
+
 	for duplicate_height in range(duplicates_height):
 		for duplicate_width in range(duplicates_width):
 			offset = (16 * duplicate_width, 16 * duplicate_height)
 			background.paste(img, offset)
-	background.save('big-palette.png')
+
+	background_px = np.reshape(background.getdata(), (background.width * background.height, 3))
+
+	np.random.shuffle(background_px)
+
+	background_px = np.reshape(background_px, (background.height, background.width, 3))
+
+	res = Image.fromarray(background_px.astype('uint8'))
+
+	res.save('big-palette.png')
 
 
 if __name__ == "__main__":
